aa171f2e19f25b5f401232c38ed50c51d8b0760b
Andrey <Andreysk8er@ukr.net>
2025-05-02 23:39:51 +0300

    Add BaseButton component

---
+++ src/components/ui/buttons/BaseButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button :class="['base-button', className]" @click="$emit('click')">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{ className?: string }>();
+defineEmits<{ (event: "click"): void }>();
+</script>
+
+<style scoped>
+.base-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 12px 16px;
+  border: none;
+  cursor: pointer;
+  height: 48px;
+  width: 100%;
+  color: #0e0f14;
+  border-radius: 8px;
+  background: #43f776;
+  transition: all 0.3s ease;
+}
+
+:deep(.base-button) {
+  all: unset;
+}
+
+.base-button:hover {
+  opacity: 0.8;
+}
+
+.base-button:active {
+  opacity: 0.6;
+}
+</style>
