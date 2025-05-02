be781ff5a8dec5c0ce3794ab3b3779dcad269715
Andrey <Andreysk8er@ukr.net>
2025-05-02 23:37:43 +0300

    Add BurgerButton component

---
+++ src/components/ui/buttons/BurgerButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="burger">
+    <div class="line"></div>
+  </div>
+</template>
+
+<script lang="ts"></script>
+
+<style lang="scss" scoped>
+.burger {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 25px;
+  height: 20px;
+  position: relative;
+  cursor: pointer;
+
+  .line,
+  .line::before,
+  .line::after {
+    content: "";
+    position: absolute;
+    width: 25px;
+    height: 3px;
+    border-radius: 25px;
+    background: #fff;
+    transition: all 0.3s ease;
+  }
+
+  .line {
+    background: #fff;
+
+    &::before {
+      transform: translateY(-8px);
+    }
+
+    &::after {
+      transform: translateY(8px);
+    }
+  }
+}
+</style>
