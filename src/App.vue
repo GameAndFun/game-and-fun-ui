cbaf172c3b6a2b33b2f5e747562d4b8fa1f9e31c
Andrey <Andreysk8er@ukr.net>
2025-05-03 14:36:42 +0300

    Integrate AuthorizationPopup into App.vue for user authentication flow

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import AppHeader from "@/components/app/AppHeader.vue";
+import AuthorizationPopup from "@/components/popups/AuthorizationPopup.vue";
+
+const isAuthorizationPopupOpen = ref(false);
+</script>
 
 <template>
-  <nav>
-    <RouterLink to="/">Go to Home</RouterLink>
-    <RouterLink to="/about">Go to About</RouterLink>
-  </nav>
+  <AppHeader @openAuthorizationPopup="isAuthorizationPopupOpen = true" />
 
   <router-view v-slot="{ Component, route }">
     <div :key="route.name">
       <component :is="Component" />
     </div>
   </router-view>
+
+  <AuthorizationPopup
+    :isOpen="isAuthorizationPopupOpen"
+    @closePopup="isAuthorizationPopupOpen = false"
+  />
 </template>
 
 <style src="@/assets/styles/main.scss"></style>
